9500464af372d819036ca42a87b86e0f953d89a8
Martijn Vegter <mail@mvegter.me>
2018-05-04 11:27:14 +0200

    Added Depressurizer.Core

---
+++ Source/Depressurizer.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,59 @@
+﻿#region License
+
+//     This file (AssemblyInfo.cs) is part of Depressurizer.
+//     Copyright (C) 2018  Martijn Vegter
+// 
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+// 
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+// 
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#endregion
+
+#region
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+#endregion
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Depressurizer.Core")]
+[assembly: AssemblyDescription("Core functionality of Depressurizer.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Martijn Vegter")]
+[assembly: AssemblyProduct("Depressurizer.Core")]
+[assembly: AssemblyCopyright("Copyright (C) 2018  Martijn Vegter")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("626ee111-c59d-4fd8-8abb-8ea373cd0256")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
